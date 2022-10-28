709a44974766f64dcdaca1c6b954ba9b78f04b7f
unai-daw <113602532+unai-daw@users.noreply.github.com>
2022-10-28 09:58:29 +0200

    views

--- store/views.py
+++ store/views.py
@@ -5,3 +5,11 @@ def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def carro(request):
+    template = loader.get_template('carro.html')
+    return HttpResponse(template.render())
+
