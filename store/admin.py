b11a202628fa91cfadf3f226cd3427c0a1110b03
MarkelUribe <75306077+MarkelUribe@users.noreply.github.com>
2022-11-03 10:51:17 +0100

    Delete store directory

--- store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Erabiltzailea)
-admin.site.register(Produktua)
-admin.site.register(Eskaera)
-admin.site.register(EskaeraLerroa)
